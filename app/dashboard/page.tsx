9dff4cc859b7026107e6c78f5eae28a7a14bb186
Gabriel Prates Bitencourt <devprates@gmail.com>
2025-03-09 19:04:04 -0400

    feat: async clerk user data with database

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,12 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
     return (
         <div>
             <h1>Bem vindo a dashboard</h1>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Navbar from "@/components/navbar";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>bem vindo </h1>
       <Button>teste</Button>
     </div>
